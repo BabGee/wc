50f2710de31d3160147029d22015871d09e5b2a7
Arita <samson.arita@interintel.co.ke>
2018-01-30 14:38:52 +0300

    Updates

--- wc/settings.py
+++ wc/settings.py
@@ -98,10 +98,35 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-#REQUEST CONTEXT PROCESSOR
-TEMPLATE_CONTEXT_PROCESSORS += (
-    'django.core.context_processors.request',
-)
+##REQUEST CONTEXT PROCESSOR
+#TEMPLATE_CONTEXT_PROCESSORS += (
+#    'django.core.context_processors.request',
+#)
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            # insert your TEMPLATE_DIRS here
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                # list if you haven't customized them:
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 
 ROOT_URLCONF = 'wc.urls'
 
