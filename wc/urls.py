d57a264f2cc5742190baf6ea1638d8a214cbe39e
Arita <samson.arita@interintel.co.ke>
2018-01-30 15:01:45 +0300

    Updates

--- wc/urls.py
+++ wc/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import *
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'wc.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
@@ -13,4 +13,4 @@ urlpatterns = patterns('',
     url(r'^api/', include('api.urls')),
     url(r'^', include('gui.urls')),
 
-)
+]
