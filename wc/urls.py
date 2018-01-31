66123a901755cfdcd6889a48640dfa76427afcc0
Arita <samson.arita@interintel.co.ke>
2018-01-31 08:23:15 +0300

    Updates

--- wc/urls.py
+++ wc/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^processor/', include('processor.urls')),
     url(r'^api/', include('api.urls')),
     url(r'^', include('gui.urls'))
