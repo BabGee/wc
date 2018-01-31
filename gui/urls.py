09649fafa617da2448fbd1033936d015aea70add
Arita <samson.arita@interintel.co.ke>
2018-01-31 07:20:46 +0300

    urls.py Updates

--- gui/urls.py
+++ gui/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import *
+from django.contrib.auth import views
 from gui.views import *
 
 urlpatterns = [
         url(r'^GOTO/(?P<SERVICE>[\w\ ]{1,45})/$',  UI().request_processor),
-	url(r'^logout(?:/(?P<template_name>[\.\w]{1,100}))?/$', 'django.contrib.auth.views.logout',
-                                  {'next_page': '/'}, name='logout'),
+	#url(r'^logout(?:/(?P<template_name>[\.\w]{1,100}))?/$', 'django.contrib.auth.views.logout',
+        #                          {'next_page': '/'}, name='logout'),
+	#url(r'^logout(?:/(?P<template_name>[\.\w]{1,100}))?/$', views.LogoutView.as_view(next_page='/')),
+	url(r'^logout/$', views.logout, {'next_page': '/'}),
 	url(r'^cybersource_termurl/$', UI().cybersource_termurl, name='cybersource_termurl'),
 	url(r'^site/(?P<subdomain>[\.\w]{1,100})/$', UI().pages, { 'page': 'site'} ),
 	url(r'^(?P<page>\w{1,30})(?:/(?P<subdomain>[\.\w]{1,100}))?/$', UI().pages ),
