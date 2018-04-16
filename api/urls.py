a3c6399b406bf01233403e7e68e6c7951345dcfd
Arita <samson.arita@interintel.co.ke>
2018-04-16 20:27:40 +0300

    Updates

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,6 @@ from django.views.generic import RedirectView
 from api.views import *
 
 urlpatterns = [
-        url(r'^pdf/(?P<names>[\w\ ]{1,200})/$',  Interface().pdf_gen),
         url(r'^facebook/(?P<names>[\w\ ]{1,200})/$',  Interface().facebook),
         url(r'^load-video-file/$',  Interface().load_video_file),
         url(r'^upload-temp-file/$',  Interface().upload_temp_file),
