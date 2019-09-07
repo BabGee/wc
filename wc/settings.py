b31ad60ef5f2262fef5bfcb1688d70c3bc147cfc
root <root@localhost.localdomain>
2019-09-07 10:22:54 +0300

    Updates

--- wc/settings.py
+++ wc/settings.py
@@ -162,7 +162,6 @@ DATABASES = {
         'PASSWORD': dbpassword,
         'HOST': dbhost,                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': dbport,                      # Set to empty string for default.
-        'CONN_MAX_AGE': 0,
     }
 }
 
