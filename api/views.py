7c6fefda1836bb0856f8d9961a9b484f37836cb1
root <root@localhost.localdomain>
2019-12-06 12:12:55 +0300

    Updates

--- api/views.py
+++ api/views.py
@@ -187,7 +187,9 @@ class Interface(Wrapper):
 			payload = {}
 			#ip_address = request.META.get('REMOTE_ADDR') 
 			#ip_address = request.META.get('CF-Connecting-IP', request.META.get('REMOTE_ADDR'))
-			ip_address = request.META.get('CF-Connecting-IP', request.META.get('HTTP_X_FORWARDED_FOR', request.META.get('REMOTE_ADDR')))
+			#ip_address = request.META.get('CF-Connecting-IP', request.META.get('HTTP_X_FORWARDED_FOR', request.META.get('REMOTE_ADDR')))
+
+			ip_address = request.META.get('CF-Connecting-IP', request.META.get('HTTP_X_FORWARDED_FOR', request.META.get('HTTP_X_REAL_IP', request.META.get('REMOTE_ADDR'))))
 			g = GeoIP2()
 			city = g.city(ip_address)
 			lgr.info('City: %s' % city)
