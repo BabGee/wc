5ce05858f77cd899008ac36720070f4a169c380c
Leonard Kiano <leonard.kiano@interintel.co.ke>
2020-01-21 17:19:00 +0300

    staff-profile type and summary tab updated

--- gui/static/prod/src/themes/dsv1.0/components/elements/datalist-element/staffprofile-type.js
+++ gui/static/prod/src/themes/dsv1.0/components/elements/datalist-element/staffprofile-type.js
@@ -188,100 +188,131 @@ import{LitElement,html}from"../../../../../components/adaptive-ui.js";class Data
 }_dateToFilter(evt){// console.log('date to filter');
 // console.log(evt);
 const picker=evt.currentTarget,x={start_date:moment.unix(picker.dateFrom).format("YYYY-MM-DD"),end_date:moment.unix(picker.dateTo).format("YYYY-MM-DD")};this.fire("filter-date-range",{range:x})}_handleFilter(){if(this.column.filter){if(this.column.activeFilter){const paperInput=this.shadowRoot.querySelector("paper-input");if(paperInput){paperInput.value=""}this.previousValue=null}this.fire("filter-th-content",{column:this.column})}}setPaperInputValue(value){this.shadowRoot.querySelector("paper-input").value=value}_handleChoiceChanged(){this.dispatchEvent(new CustomEvent("input-change-th-content",{detail:{column:this.column,value:this._selectedChoices}}))}_handleColumnDropdownFilter(evt){this.dispatchEvent(new CustomEvent("input-change-th-content",{detail:{column:this.column,value:evt.detail.item.val}}))}_handleActiveFilterChange(event){const parentDiv=event.currentTarget.parentNode;Polymer.dom.flush();this.async(()=>{let paperInput;if(!this.column.date&&!this.column.choices){paperInput=this.shadowRoot.querySelector("paper-input");if(paperInput){paperInput.focus();if(this.column.activeFilterValue){this.previousValue=this.column.activeFilterValue}}}else if(this.column.date){const datePicker=parentDiv.querySelector("range-datepicker-input");if(datePicker){if(this.column.activeFilterValue){this.previousValue=this.column.activeFilterValue}}}else{this._selectedChoices=[]}})}_handleKeyDownInput(event){const input=event.currentTarget;this.currentValue=input.value;if(this.previousValue!==this.currentValue){if(13===event.keyCode){this.fire("input-change-th-content",{column:this.column,value:this.currentValue});this.previousValue=this.currentValue}else{clearTimeout(this.timeoutFilter);this.timeoutFilter=setTimeout(()=>{if(this.previousValue!==this.currentValue){this.fire("input-change-th-content",{column:this.column,value:this.currentValue})}this.previousValue=this.currentValue},1e3)}}}equals(targetedValue,value){return value===targetedValue}_draggableClass(draggable){if(draggable){return"draggable"}return""}_isDraggable(draggableColumn,focused){if(draggableColumn&&!focused){return"true"}return"false"}_computeIconName(choice,selectedChoices){if(-1===selectedChoices.base.indexOf(choice)){return"check-box-outline-blank"}return"check-box"}}customElements.define(DataSourceStaffProfileHead.is,DataSourceStaffProfileHead);var datasourceStaffprofileHead={DataSourceStaffProfileHead:DataSourceStaffProfileHead};class StaffProfileTypeFooter extends LitElement{render(){return html`
-        <style>
-            :host {
-                display: block;
-            }
+    <style>
+    :host {
+      display: block;
+    }
+    .size{
+      margin-right: 15px;
+    }
+    .btn{
+      position: relative;
+      cursor: pointer;
+      top: 4px;
+      left: 20px;
+    }
+    .right-btn::after, .left-btn::after{
+      content: '';
+      position: absolute;
+      width: 7px;
+      height: 7px;
+      border-left: 2px solid #3a3a3a;
+      border-bottom: 2px solid #3a3a3a;
+    }
+    .right-btn{
+      position: relative;
+    }
+    .left-btn::after{
+      transform: rotate(45deg);
+    }
+    .right-btn::after{
+      left: 15px;
+      transform: rotate(-135deg);
+    }
+    .perpage-dropdown .p-display{
+      position: relative;
+      cursor: pointer;
+      margin: 0 15px 0 0;
+    }
+    .perpage-dropdown .p-display::after{
+     
+    }
+    .per{
+      position:relative;
+      
+    }
+    .per::after{
+      content:'';
+      position: absolute;
+      right: -14px;
+      top: 4px;
+      width: 5px;
+      height: 5px;
+      border-left: 2px solid #3a3a3a;
+      border-bottom: 2px solid #3a3a3a;
+      transform: rotate(-45deg);
+    }
+    .perpage-dropdown .p-dropdown{
+      width: 100%;
+      background: #fff;
+      border: 1px solid #e5e5e5;
+      padding: 0 8px;
+      display: none;
+    }
+    .perpage-dropdown .p-dropdown li a{
+      text-align: right;
+      display: inline-block;
+      width: 100%;
+      color: #4a4a4a;
+    }
+    .perpage-dropdown .p-dropdown li a:hover{
+      background: #e5e5e5;
+    }
+    .active-list{
+      background: #e5e5e5;
+    }
+</style>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
+<div class="wrapper is-flex" style="margin-top: 20px;">
+  <div class="size">
+  
+    <div class="perpage-dropdown">
+      <div class="p-display" >
+      
+        ${this.availableSize.length?html`
+        <p class="per is-capitalized is-size-7" @click=${this._dropdownReveal}>Per Page ${this.size}</p>
+        <div class="p-dropdown">
+        <ul>
+          ${this.availableSize.map(size=>html`<li class="${size==this.size?"active-list":""}"><a href="$1" class="is-size-7" data-name="${size}"  @click="${this._newSizeIsSelected}">${size}</a></li>`)}
+          
+        </ul>
+        </div>
+        `:html`<p class="is-capitalized is-size-7">Per Page ${this.size}</p>`}
+      </div>
 
-            .foot  {
-                font-size: 12px;
-                font-weight: normal;
-                height: 55px;
-                border-top: 1px solid;
-                border-color: rgba(0, 0, 0, var(--dark-divider-opacity));
-                padding: 0 14px 0 0;
-                color: rgba(0, 0, 0, var(--dark-secondary-opacity));
-            }
+    </div>
+  </div>
+  <div class="range">
+    <div class="is-flex">
+      <div class="pagination-range">
+        <p class="is-size-7">${this._computeCurrentSize(this.page,this.size)} - ${this._computeCurrentMaxSize(this.page,this.size,this.totalElements)} of ${this.totalElements}</p>
+      </div>
+      <div class="is-flex pagination-buttons">
+      ${this._prevButtonEnabled(this.page)?html`<div class="left-btn btn" @click="${this._prevPage}" ?disabled="${!this._prevButtonEnabled(this.page)}"></div>`:html`<div></div>`}
+        <div class="right-btn btn" @click="${this._nextPage}" ?disabled="${!this._nextButtonEnabled(this.page,this.totalPages)}"></div>
+      </div>
+    </div>
+  </div>
+</div>`}constructor(){super();this.availableSize=[];this.size=50}static get is(){return"staffprofile-type-footer"}static get properties(){return{footerPosition:String,size:{type:Number},page:{type:Number},totalElements:{type:Number},totalPages:{type:Number},availableSize:Array}}_dropdownReveal(){const drp=this.shadowRoot.querySelector(".p-dropdown");switch(drp.style.display){case"block":drp.style.display="none";break;default:drp.style.display="block";break;}}_computeCurrentSize(page,size){return(page-1)*size+1}_computeCurrentMaxSize(page,size,totalElements){const maxSize=size*page;return maxSize>totalElements?totalElements:maxSize}_nextPage(){if(this.page<this.totalPages){this.page=this.page+1}this.dispatchEvent(new CustomEvent("n-page",{detail:{page:this.page}}))}_prevPage(){if(0<this.page-1){this.page=this.page-1}this.dispatchEvent(new CustomEvent("p-page",{detail:{page:this.page}}))}_nextButtonEnabled(page,totalPages){return page<totalPages}_prevButtonEnabled(page){return 1<page}_newSizeIsSelected(){const newSize=this.shadowRoot.querySelector("paper-listbox").selected;if(newSize){if(null!==this.oldPage&&this.oldPage!==void 0){this.page=1}this.size=newSize;this.dispatchEvent(new CustomEvent("size-change",{detail:{size:newSize}}))}}_computePosition(position){if("right"===position){return"end-justified"}return""}}customElements.define(StaffProfileTypeFooter.is,StaffProfileTypeFooter);var staffprofileTypeFooter={StaffProfileTypeFooter:StaffProfileTypeFooter};class StaffProfileHeader extends LitElement{render(){return html`
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
+    <style>
+      .table-header-buttons{
+          width: 100%;
+          height: auto;
+          position: relative;
+      }
 
-            .foot .left  {
-                padding: 0 0 0 14px;
-            }
-
-            .foot paper-icon-button {
-                width: 24px;
-                height: 24px;
-                padding: 0;
-                margin-left: 24px;
-            }
-
-            .foot .status {
-                margin: 0 8px 0 32px;
-            }
-
-            .foot .size {
-                width: 64px;
-                text-align: right;
-            }
-
-            .size paper-dropdown-menu {
-                --paper-input-container-underline: {
-                    display: none;
-                };
-                --paper-input-container-input: {
-                    text-align: right;
-                    font-size: 12px;
-                    font-weight: 500;
-                    color: var(--app-default-color, rgba(0, 0, 0, .54));
-                };
-                --paper-dropdown-menu-icon: {
-                    color: var(--app-default-color, rgba(0, 0, 0, .54));
-                };
-            }
-        </style>
-        <div class$="layout horizontal center foot [[_computePosition(footerPosition)]]">
-            <div class$="[[footerPosition]]">
-                <div class="layout horizontal end-justified center">
-                    <div class="layout horizontal center" style="display: inline-block;">
-                        <div style="text-align: right;">
-                            Per Page
-                        </div>
-                        <div class="size">
-                          ${this.availableSize.length?html`
-                                <paper-dropdown-menu no-label-float vertical-align="bottom">
-                                    <paper-listbox attr-for-selected="size"
-                                                   @iron-select="${this._newSizeIsSelected}"
-                                                   selected="${this.size}"
-                                                   slot="dropdown-content">
-                                        ${this.availableSize.map(size=>html`<paper-item size="${size}">${size}</paper-item>`)}
-                                    </paper-listbox>
-                                </paper-dropdown-menu>
-                          `:html`
-                                <span>50</span>
-                          `}                            
-                        </div>
-                    </div>
-
-                    <div class="status" style="display: inline-block;">
-                        ${this._computeCurrentSize(this.page,this.size)} - ${this._computeCurrentMaxSize(this.page,this.size,this.totalElements)} of ${this.totalElements}
-                    </div>
-                    
-                    <paper-icon-button icon="chevron-left" 
-                                       ?disabled="${!this._prevButtonEnabled(this.page)}" 
-                                       @click="${this._prevPage}"></paper-icon-button>
-                                       
-                    <paper-icon-button icon="chevron-right" 
-                                       ?disabled="${!this._nextButtonEnabled(this.page,this.totalPages)}" 
-                                       @click="${this._nextPage}"></paper-icon-button>
-                
-                </div>
-            </div>
-        </div>`}constructor(){super();this.availableSize=[];this.size=50}static get is(){return"staffprofile-type-footer"}static get properties(){return{footerPosition:String,size:{type:Number},page:{type:Number},totalElements:{type:Number},totalPages:{type:Number},availableSize:Array}}_computeCurrentSize(page,size){return(page-1)*size+1}_computeCurrentMaxSize(page,size,totalElements){const maxSize=size*page;return maxSize>totalElements?totalElements:maxSize}_nextPage(){if(this.page<this.totalPages){this.page=this.page+1}this.dispatchEvent(new CustomEvent("n-page",{detail:{page:this.page}}))}_prevPage(){if(0<this.page-1){this.page=this.page-1}this.dispatchEvent(new CustomEvent("p-page",{detail:{page:this.page}}))}_nextButtonEnabled(page,totalPages){return page<totalPages}_prevButtonEnabled(page){return 1<page}_newSizeIsSelected(){const newSize=this.shadowRoot.querySelector("paper-listbox").selected;if(newSize){if(null!==this.oldPage&&this.oldPage!==void 0){this.page=1}this.size=newSize;this.dispatchEvent(new CustomEvent("size-change",{detail:{size:newSize}}))}}_computePosition(position){if("right"===position){return"end-justified"}return""}}customElements.define(StaffProfileTypeFooter.is,StaffProfileTypeFooter);var staffprofileTypeFooter={StaffProfileTypeFooter:StaffProfileTypeFooter};class StaffProfileHeader extends LitElement{render(){return html`
-
-        <h4>${this.title}</h4>
-        <div>
-       <paper-button  @tap="${this.generatePDF}" style=" background: #3498db; color:white;"><iron-icon icon="image:picture-as-pdf"></iron-icon>Export PDF</paper-button>
-       <paper-button @tap="${this.generateCSV}" style=" background: #2ecc71; color:white;"><iron-icon icon="icons:save"></iron-icon>Export CSV</paper-button>
-   </div>
+      .table-header-buttons h1{
+        margin-bottom: 15px;
+      }
+    </style>
+    <div class="table-header-buttons" style="margin-bottom: 15px;">
+      <div class="heading">
+        <h1 class="title is-size-6">${this.title}</h1>
+      </div>
+      <button @tap="${this.generatePDF}" class="button is-info is-size-7 is-rounded">Export PDF</button>
+      <button @tap="${this.generateCSV}" class="button is-success is-size-7 is-rounded">Export CSV</button>
+    </div>
 `}static get is(){return"staffprofile-type-header"}static get properties(){return{title:String//   language: String,
 //   column: {
 //     type: Object,
@@ -379,7 +410,7 @@ tr {
           <tr>
             <th style="padding-top: 20px; padding-bottom: 20px;" class="arrow">
               
-                    <p style="font-weight: bold; font-size: 16px; font-family: Montserrat; color: #202124;" >Short code</p>
+                    <p style="font-weight: bold; font-size: 16px;  color: #202124;" >Short code</p>
             </th>
 
             <th style="padding-top: 20px; padding-bottom: 20px;">
@@ -401,7 +432,7 @@ tr {
 
             <th style="padding-top: 20px; padding-bottom: 20px;">
 
-                    <p @click="${this.doService}" style="font-weight: bold; font-size: 16px; font-family: Montserrat; color: #2f75ec;" 
+                    <p @click="${this.doService}"  style="font-weight: bold; font-size: 16px;  color: var(--app-primary-color);" 
                     class="is-pulled-right pointer">
 
                             <span class="icon" style="color: #000;">
@@ -442,7 +473,7 @@ tr {
           <tr id="tr-${itemIndex}">
           <th class="index-column" style="padding-top: 20px; padding-bottom: 20px;">
 
-          ${!0!==this.details.selectable?html`<span class= "normal-td" style="color: #6c7a89; font-size: 14px; font-weight: 500; font-family: Montserrat;">${item.index}</span>
+          ${!0!==this.details.selectable?html`<span class= "normal-td" style="color: #6c7a89; font-size: 14px; font-weight: 500; ">${item.index}</span>
           `:html`<label class="checkbox">
           <input id="checkbox-${itemIndex}" @click="${()=>this.checkRow(itemIndex)}" type="checkbox">
           
@@ -455,7 +486,7 @@ tr {
 
               <td class="normal-td" style="padding-top: 20px; padding-bottom: 20px;">
 
-                      <span class= "normal-td" style="color: #6c7a89; font-size: 14px; font-weight: 500; font-family: Montserrat;">${item.name}</span>
+                      <span class= "normal-td" style="color: #6c7a89; font-size: 14px; font-weight: 500; ">${item.name}</span>
               </td>
 
               <td " style="padding-top: 20px; padding-bottom: 20px;"> 
@@ -465,7 +496,7 @@ tr {
 
               <td  style="padding-top: 20px; padding-bottom: 20px;">
                   
-                  <span style="color: #6c7a89; font-size: 14px; font-weight: 500; font-family: Montserrat;" class= "normal-td">${item["Contact Count"]}</span>
+                  <span style="color: #6c7a89; font-size: 14px; font-weight: 500; " class= "normal-td">${item["Contact Count"]}</span>
 
               
               </td>
