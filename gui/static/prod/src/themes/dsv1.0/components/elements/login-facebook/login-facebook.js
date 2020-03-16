e7d341cd4a40d3d328b6f9a1c05da002cff685da
Leonard Kiano <leonard.kiano@interintel.co.ke>
2020-03-16 10:01:40 +0300

    build has improved design on pinpad element and log-in facebook and google

--- gui/static/prod/src/themes/dsv1.0/components/elements/login-facebook/login-facebook.js
+++ gui/static/prod/src/themes/dsv1.0/components/elements/login-facebook/login-facebook.js
@@ -1,26 +1,62 @@
 import{SerializableElement,css,html}from"../../../../../components/adaptive-ui.js";const LoginFacebookBase=class extends SerializableElement{static get is(){return"login-facebook"}constructor(){super();(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src="//connect.facebook.net/en_US/sdk.js";fjs.parentNode.insertBefore(js,fjs)})(document,"script","facebook-jssdk");this.addEventListener("click",this._submit.bind(this))}static get properties(){return{title:String,name:String,type:String,params:{type:Object,value:{}}}}firstUpdated(changedProperties){super.firstUpdated(changedProperties)}init(pElement,loader){super.init(pElement,loader);this;this.name=pElement.name;window.fbAsyncInit=function(){window.FB.init({appId:pElement.defaultValue,cookie:!0,xfbml:!0,version:"v3.1"})}}facebookLogin(){window.FB.login(function(resp){this.statusChangeCallback(resp)}.bind(this),{scope:"email,public_profile"})}checkLoginState(){alert("Checking Login Status");console.log("Checking login status...........");window.FB.getLoginStatus(function(response){alert("FB Callback");console.log("----------->");console.log(response);this.statusChangeCallback(response)}.bind(this))}statusChangeCallback(response){console.log("statusChangeCallback");console.log(response);if("connected"===response.status){this.value=response.authResponse.accessToken;this.pl.submitForm()}else if("not_authorized"===response.status){console.log("Import error","Authorize app to import data","error")}else{console.log("Import error","Error occured while importing data","error")}}fetchDataFacebook(){const self=this;console.log("Welcome!  Fetching your information.... ");window.FB.api("/me?fields=id,name,email",function(user){console.log(user);console.log("Successful login from facebook : "+user.name);const value={name:user.name,email:user.email};self.value=value;self.pl.submitForm()})}_submit(){this.facebookLogin()}getName(){return this.e.formName}getValue(){return this.value}validate(){return new this.Validation(!0,"valid")}invalid(){}valid(){}};var loginFacebook={LoginFacebookBase:LoginFacebookBase};const LoginFacebookStyles=css`
-.icon:hover{
-    z-index: 1;
-    opacity: 0.8;
-    border-radius: 100%;
-    transform: scale(2.0);
-    transition: all 0.3s;
+
+.gplus, .facebook-icon {
+    background-color: #DB4F2E;
+    padding: 12px;
+    margin-left: -4px;
 }
+
+.facebook-icon{
+  background-color: #395D9B !important;
+}
+
+    .gplus-icon, .facebook-iconic{
+        
+        background-color: #CE3D28;
+        
+        margin-top: 15px;
+        
+    }
+
+    .facebook-iconic{
+      background-color: #354F88 !important;
+    }
+
+    .hero-body a {
+        color: #fff;
+    }
+
+    .gplus-i, .facebook-i {
+
+      background-color: #CE3D28;
+        
+        padding: 12px;
+    }
+
+    .facebook-i {
+      background-color: #354F88 !important;
+    }
+
+    fa-icon.fab.fa-facebook-f.gplus-icon.facebook-iconic {
+
+        margin-bottom: -4px !important;
+        
+        
+        }
 
 `;var loginFacebookCss={LoginFacebookStyles:LoginFacebookStyles};class LoginFacebook extends LoginFacebookBase{static get styles(){return[LoginFacebookStyles,css`
         :host {
           display: block;
         }
       `]}renderDefault(){return html`
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
       
-                 <div class="column">
-        <div class="field">
-        <a class="button is-rounded is-medium" style="background-color: #3578E5; color:#fff; width: 40px; height: 60px;">
-    <span class="icon">
-      <i><iron-icon icon="social:facebook"></iron-icon></i>
-    </span>
-  </a>
-        </div>
-        </div>
+    <section class="hero">
+    <div class="hero-body">
+
+        <a><span class="gplus-i facebook-i"><fa-icon class="fab fa-facebook-f gplus-icon facebook-iconic " style ="fill: #fff; margin-bottom: -4px;"></fa-icon></span> <span class="gplus facebook-icon">Login in with Facebook</span></a>
+
+    </div>
+</section>
         `}constructor(){super()}}customElements.define(LoginFacebook.is,LoginFacebook);export{loginFacebook as $loginFacebook,loginFacebookCss as $loginFacebookCss,LoginFacebookBase,LoginFacebookStyles};
--- gui/static/prod/src/themes/dsv1.0/components/elements/pinpad-input/pinpad-input.js
+++ gui/static/prod/src/themes/dsv1.0/components/elements/pinpad-input/pinpad-input.js
@@ -2,15 +2,17 @@ import{SerializableElement,css,html,InputDefaultStyles,InputLabelAnimationtStyle
 .pinpad-dropdown{
     position:absolute;
     z-index:9999;
-    width: 100%;
+    width: 97.8%;
     background-color: #fff;
     border: 1px solid rgb(183, 181, 181);
+    border-radius: 7px;
     padding: 10px;
     display: none;
 }
 .pin-btn{
     width: 100%;
     border: 2px solid rgb(183, 181, 181);
+    border-radius: 7px;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -46,6 +48,118 @@ import{SerializableElement,css,html,InputDefaultStyles,InputLabelAnimationtStyle
     position: absolute;
     right: 12px;
     top: 60px;
+    color:#cecece;
+}
+
+@media screen and (max-width: 1024px){ 
+
+    .pinpad-dropdown{ 
+        width: 97.4%;
+    }
+
+}
+
+@media screen and (max-width: 1081px){ 
+
+    .pinpad-dropdown{ 
+        width: 97.5%;
+    } 
+}
+
+@media screen and (max-width: 1013px){ 
+
+    .pinpad-dropdown{ 
+        width: 97.2%;
+    } 
+}
+
+
+
+@media screen and (max-width: 810px){ 
+
+    .pinpad-dropdown{ 
+        width: 96.7%;
+    } 
+}
+
+@media screen and (max-width: 772px){ 
+
+    .pinpad-dropdown{ 
+        width: 96.4%;
+    } 
+}
+
+@media screen and (max-width: 768px){ 
+
+    .pinpad-dropdown{ 
+        width: 96%;
+    }
+
+}
+
+@media screen and (max-width: 727px){ 
+
+    .pinpad-dropdown{ 
+        width: 95.9%;
+    } 
+}
+
+@media screen and (max-width: 652px){ 
+
+    .pinpad-dropdown{ 
+        width: 95.4%;
+    } 
+}
+
+@media screen and (max-width: 593px){ 
+
+    .pinpad-dropdown{ 
+        width: 94.9%;
+    } 
+}
+
+@media screen and (max-width: 546px){ 
+
+    .pinpad-dropdown{ 
+        width: 94.2%;
+    } 
+}
+
+@media screen and (max-width: 414px){ 
+
+    .pinpad-dropdown{ 
+        width: 91%;
+    }
+
+}
+
+
+@media screen and (max-width: 375px){ 
+
+    .pinpad-dropdown{ 
+        width: 90%;
+    }
+
+}
+
+@media screen and (max-width: 360px){ 
+
+    .pinpad-dropdown{ 
+        width: 89.8%;
+    }
+
+}
+
+@media screen and (max-width: 320px){ 
+
+    .pinpad-dropdown{ 
+        width: 87.9%;
+    }
+
+    .the-third{
+        margin-left: -5px;
+    }
+
 }
 
 `;var pinpadInputCss={PinpadInputStyles:PinpadInputStyles};class PinPadInput extends PinPadInputBase{static get styles(){return[PinpadInputStyles,InputDefaultStyles,InputLabelAnimationtStyles,css`
@@ -81,7 +195,7 @@ import{SerializableElement,css,html,InputDefaultStyles,InputLabelAnimationtStyle
         </div>
           <div class="message-block">
             <p class="help" id="warning-text" style="color:#ff3860;">${this.e.name} required</p>
-            <p id ="count" class="input-counter">${this.e.max}</p>
+            <p id ="count" class="input-counter">0/${this.e.max}</p>
           </div>
 
       </div>
@@ -98,7 +212,7 @@ import{SerializableElement,css,html,InputDefaultStyles,InputLabelAnimationtStyle
               <p class="key-num">2</p>
             </div>
           </div>
-          <div class="column is-4">
+          <div class="column is-4 the-third">
             <div class="pin-btn" @click=${this.pinBtn}>
               <p class="key-num">3</p>
             </div>
@@ -113,7 +227,7 @@ import{SerializableElement,css,html,InputDefaultStyles,InputLabelAnimationtStyle
               <p class="key-num">5</p>
             </div>
           </div>
-          <div class="column is-4">
+          <div class="column is-4 the-third">
             <div class="pin-btn" @click=${this.pinBtn}>
               <p class="key-num">6</p>
             </div>
@@ -128,7 +242,7 @@ import{SerializableElement,css,html,InputDefaultStyles,InputLabelAnimationtStyle
               <p class="key-num">8</p>
             </div>
           </div>
-          <div class="column is-4">
+          <div class="column is-4 the-third">
             <div class="pin-btn" @click=${this.pinBtn}>
               <p class="key-num">9</p>
             </div>
@@ -146,7 +260,7 @@ import{SerializableElement,css,html,InputDefaultStyles,InputLabelAnimationtStyle
             </div>
           </div>
 
-          <div class="column is-4">
+          <div class="column is-4 the-third">
             <div class="pin-btn" @click=${this.submit}>
               <p><fa-icon class="fas fa-check" color="#23D160"></fa-icon></p>
             </div>
