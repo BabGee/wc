b19a7eb2ab86a69969e00fd3261ed4856595f364
root <root@localhost.localdomain>
2021-06-22 10:32:06 +0300

    Updates

--- api/views.py
+++ api/views.py
@@ -146,6 +146,7 @@ class Interface(Wrapper):
 					obj = str(file_object.content_type).split("/")[0]
 					lgr.info('Object is: %s' % obj)
 					if  (int(file_object.size) > 25000000 and obj in ['image','text']) or \
+					(int(file_object.size) > 25000000 and file_object.content_type in ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet']) or \
 					(int(file_object.size) > 25000000 and obj in ['audio']) or \
 					obj not in ['image','text','audio'] and file_object.content_type \
 									not in ['application/vnd.ms-excel','application/pdf']:
